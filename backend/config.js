00311ab612e5201fe81c59dfe6c0a4aa688a3191
Nguyen Duy Khanh <khanhnd95.ptit@gmail.com>
2023-10-12 15:55:28 +0700

    chore: Add database connection

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://root:root@book-store-mern.adcxuco.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+    console.log('Database is connected');
+    app.get('/', (request, response) => {
+        console.log(request);
+        return response.status(234).send('Welcome To MERN Stack Tutorial');
+    });
+})
+.catch((error) => {
+    console.log(error);
+}); 
 
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
