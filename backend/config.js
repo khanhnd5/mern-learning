e91085269596fa0b742c8cd6bc3807949a0df1c3
Nguyen Duy Khanh <khanhnd95.ptit@gmail.com>
2023-10-12 17:27:32 +0700

    chore: modify config to use local db

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 
-    'mongodb+srv://root:root@book-store-mern.adcxuco.mongodb.net/books-collection?retryWrites=true&w=majority'
+    'mongodb://127.0.0.1:27017/books-collection'
--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,18 @@ app.get('/books', async (request, response) => {
     }
 });
 
+// Route for getting a book by id
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+    } catch(error) {
+        console.log(error.message);
+        return response.status(500).send({message : error.message});
+    }
+});
+
 // Route for saving a new book
 app.post('/books', async (request, response) => {
     try {
